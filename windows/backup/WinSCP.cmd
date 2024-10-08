150a0f623a0b22401277348365a721222882b387
Dmitrii <dima.zs@icloud.com>
2024-10-08 22:04:03 +0300

    msys64

---
+++ windows/backup/WinSCP.cmd
@@ -0,0 +1,4 @@
+:: regedit /e "%CD%\putty-sessions.reg" HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions
+:: regedit /e "%CD%\putty.reg" HKEY_CURRENT_USER\Software\SimonTatham
+
+regedit /e "%CD%\WinSCP.reg" "HKEY_CURRENT_USER\Software\Martin Prikryl\WinSCP 2"
---
+++ windows/msys64/install.sh
@@ -0,0 +1,11 @@
+pacman -S git
+pacman -S zsh mingw-w64-x86_64-starship
+pacman -S mingw-w64-ucrt-x86_64-fzf mingw-w64-ucrt-x86_64-zoxide mingw-w64-ucrt-x86_64-bat
+
+# msys64 shell
+
+# https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
+bash -c "$(curl --fail --show-error --silent \
+    --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+
+# Windows Defender add zsh, starship, etc
